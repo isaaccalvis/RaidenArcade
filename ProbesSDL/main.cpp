f3085a9f865198dfe6c89f2387b097e9fd6f222d
isaac calvis <isaaccalvis@gmail.com>
2017-03-09 21:06:58 +0100

    Add files via upload

--- ProbesSDL/main.cpp
+++ ProbesSDL/main.cpp
@@ -4,76 +4,118 @@
 #pragma comment (lib, "lib/x86/SDL2.lib")
 #pragma comment (lib, "lib/x86/SDL2main.lib")
 
+#define WINDOW_AMPLADA 640
+#define WINDOW_LLARGADA 480
+#define NUMERO_BALES 2
+
 SDL_Window* g_pWindow = 0;
 SDL_Renderer* g_pRenderer = 0;
+SDL_Surface* texturaCarregant;
+SDL_Texture* img_Nau;
 
-SDL_Rect cuadrat = { 0, 0, 60, 60 };
-SDL_Event tecla;
+SDL_Rect PROTA = { 0, 0, 60, 60 };
+SDL_Rect DISPAR[2] = { 0, 0, 10, 10 };
+SDL_Event TECLA;
+
+int ingame = 0, moventUp = 0, moveRight = 0, numBala = 0;
+
+void InicialitzacionsVaries();
+void Renders();
+void PolliMoviments();
+void shoot(int numBala);
 
 int main(int argc, char* argv[]) {
-	SDL_Init(SDL_INIT_EVERYTHING);
-	g_pWindow = SDL_CreateWindow("Finestra1", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
-	g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
-	int ingame = 0;
+	InicialitzacionsVaries();
 	while (ingame == 0) {
-		while (SDL_PollEvent(&tecla)) {
-			switch (tecla.key.keysym.sym) {
-			case SDLK_UP:
-				if (cuadrat.y > 0)
-					cuadrat.y = cuadrat.y - 20;
-				else
-					cuadrat.y = 0;
-				break;
-			case SDLK_DOWN:
-				if (cuadrat.y < 480 - cuadrat.h)
-					cuadrat.y = cuadrat.y + 20;
-				else
-					cuadrat.y = 480 - cuadrat.h;
-				break;
-			case SDLK_RIGHT:
-				if (cuadrat.x < 640 - cuadrat.w)
-					cuadrat.x = cuadrat.x + 20;
-				else
-					cuadrat.x = 640 - cuadrat.w;
-				break;
-			case SDLK_LEFT:
-				if (cuadrat.x > 0)
-					cuadrat.x = cuadrat.x - 20;
-				else
-					cuadrat.x = 0;
-				break;
-			// Increment i decrement de tamany
-			case SDLK_q:
-				cuadrat.w = cuadrat.w + 5;
-				break;
-			case SDLK_a:
-				cuadrat.w = cuadrat.w - 5;
-				break;
-			case SDLK_w:
-				cuadrat.h = cuadrat.h + 5;
-				break;
-			case SDLK_s:
-				cuadrat.h = cuadrat.h - 5;
-				break;
-			// Per acabar execucio
-			case SDLK_p:
-				ingame = 1;
+		PolliMoviments();
+		Renders();
+		SDL_Delay(2);
+	}
+	SDL_Quit();
+	return 0;
+}
+
+void InicialitzacionsVaries() {
+	SDL_Init(SDL_INIT_EVERYTHING);
+	g_pWindow = SDL_CreateWindow("Finestra1", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_AMPLADA, WINDOW_LLARGADA, SDL_WINDOW_SHOWN);
+	g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
+
+	DISPAR[1] = DISPAR[0];
+	// Render de Imagetes BMP
+	texturaCarregant = SDL_LoadBMP("nau.bmp");
+	img_Nau = SDL_CreateTextureFromSurface(g_pRenderer, texturaCarregant);
+	SDL_FreeSurface(texturaCarregant);
+}
+void shoot(int numBala) {
+	DISPAR[numBala].x = PROTA.x + (PROTA.w / 2) - DISPAR[numBala].w;
+	DISPAR[numBala].y = PROTA.y;
+}
+void Renders() {
+	SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 255, 0);
+	SDL_RenderClear(g_pRenderer);
+	for (int i = 0; i < NUMERO_BALES; i++) {
+		SDL_SetRenderDrawColor(g_pRenderer, 0, 255, 0, 0);
+		SDL_RenderFillRect(g_pRenderer, &DISPAR[i]);
+	}
+	SDL_RenderCopy(g_pRenderer, img_Nau, NULL, &PROTA);	// Imatge de la Nau Prota
+
+	SDL_RenderPresent(g_pRenderer);
+}
+void PolliMoviments() {
+	// Crida de tecles
+	while (SDL_PollEvent(&TECLA)) {
+		if (TECLA.type == SDL_KEYDOWN && TECLA.key.repeat == 0) {
+			switch (TECLA.key.keysym.sym) {
+			case SDLK_UP: moventUp = 1; break;
+			case SDLK_DOWN: moventUp = -1; break;
+			case SDLK_RIGHT: moveRight = 1; break;
+			case SDLK_LEFT: moveRight = -1; break;
+			case SDLK_p: ingame = 1; break; // Sortir del ingame
+			}
+		}
+		if (TECLA.type == SDL_KEYUP && TECLA.key.repeat == 0) {
+			switch (TECLA.key.keysym.sym) {
+			case SDLK_UP: moventUp = 0; break;
+			case SDLK_DOWN: moventUp = 0; break;
+			case SDLK_RIGHT: moveRight = 0; break;
+			case SDLK_LEFT: moveRight = 0; break;
+			}
+		}
+		if (TECLA.key.repeat == 0 && TECLA.type == SDL_KEYUP) {
+			switch (TECLA.key.keysym.sym) {
+			case SDLK_SPACE:
+				numBala++;
+				if (numBala >= NUMERO_BALES)
+					numBala = 0;
+				shoot(numBala);
 				break;
 			}
 		}
-
-		SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 255, 0);
-		SDL_RenderClear(g_pRenderer);
-
-		SDL_SetRenderDrawColor(g_pRenderer, 255, 0, 0, 0);
-		SDL_RenderFillRect(g_pRenderer, &cuadrat);
-		SDL_RenderPresent(g_pRenderer);
-
-		std::cout << "coor x: "<<cuadrat.x << "\t coor y: " << cuadrat.y << std::endl;
 	}
 
-	SDL_Delay(1000);
-	SDL_Quit();
+	// Moviments
+	if (moventUp == 1)
+		if (PROTA.y > 0)
+			PROTA.y--;
+		else
+			PROTA.y = 0;
+	else if (moventUp == -1)
+		if (PROTA.y < WINDOW_LLARGADA - PROTA.h)
+			PROTA.y++;
+		else
+			PROTA.y = WINDOW_LLARGADA - PROTA.h;
 
-	return 0;
+	if (moveRight == 1)
+		if (PROTA.x < WINDOW_AMPLADA - PROTA.w)
+			PROTA.x++;
+		else
+			PROTA.x = WINDOW_AMPLADA - PROTA.w;
+	else if (moveRight == -1)
+		if (PROTA.x > 0)
+			PROTA.x--;
+		else
+			PROTA.x = 0;
+	// Moviments Bales
+	for (int i = 0; i < NUMERO_BALES; i++)
+		DISPAR[i].y -= 5;
 }
