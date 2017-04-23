2b8234f75f353e73c6ffa7926d33b7b1ad7f92ad
akis <isaaccalvis@gmail.com>
2017-04-23 12:30:13 +0200

    Some changes MenuScreen

--- Code/Object_Box.cpp
+++ Code/Object_Box.cpp
@@ -7,13 +7,13 @@
 #include "ModuleEnemies.h"
 
 Object_Box::Object_Box(int x, int y) : Enemy(x, y) {
-	Object_Box_Animation.PushBack({ 11, 15, 24, 24 });
+	Object_Box_Animation.PushBack({ 0, 0, 25, 23 });
 	Object_Box_Animation.speed = 0.2f;
 	Object_Box_Animation.loop = false;
 	animation = &Object_Box_Animation;
 
 	vida = 1;
-	collider = App->collision->AddCollider({ 0, 0, 24, 24 }, COLLIDER_TYPE::COLLIDER_OBJECT_COLLISION, (Module*)App->enemies);
+	collider = App->collision->AddCollider({ 0, 0, 25, 23 }, COLLIDER_TYPE::COLLIDER_OBJECT_COLLISION, (Module*)App->enemies);
 	//original_y = y;
 }
 
